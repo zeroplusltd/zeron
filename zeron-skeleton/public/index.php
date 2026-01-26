c244f062f76c00120d99801ae1b31c1e833919eb
Zeroplus Ltd. <50990684+zeroplusltd@users.noreply.github.com>
2026-01-27 02:22:03 +0600

    Update index.php

--- zeron-skeleton/public/index.php
+++ zeron-skeleton/public/index.php
@@ -1,3 +1,3 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../.zwm/modules/zeron/core');
+set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/../.lem/modules/zeron/core');
 require 'TemplateRenderer.php';
